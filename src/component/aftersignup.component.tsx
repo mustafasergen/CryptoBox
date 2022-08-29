76801e1a16b269a2e3ea66ef02bc5bf6a16a268a
Hasan Yiğit Kaya <hasanyigit.kaya@bahcesehir.edu.tr>
2022-08-29 20:08:56 +0300

    api call

--- src/component/aftersignup.component.tsx
+++ src/component/aftersignup.component.tsx
@@ -3,160 +3,171 @@ import { Layout, Avatar, Menu, MenuProps, Card } from 'antd';
 import { Button } from 'antd';
 import { Link, useNavigate } from 'react-router-dom';
 import { Image } from 'antd';
-import  { BellOutlined, ExportOutlined, SearchOutlined,  SendOutlined,  TeamOutlined,  VerticalAlignBottomOutlined,  VerticalAlignTopOutlined,  WalletOutlined } from '@ant-design/icons';
+import { BellOutlined, ExportOutlined, SearchOutlined, SendOutlined, TeamOutlined, VerticalAlignBottomOutlined, VerticalAlignTopOutlined, WalletOutlined } from '@ant-design/icons';
 import { WalletService } from '../services/wallet-service';
 import { useEffect, useState } from 'react';
+import axios from "axios";
 
 const { Header, Footer, Sider, Content } = Layout;
 
 
 
-
+const endpoint = "https://api-rinkeby.etherscan.io/api"
+const ETHERSCAN_API_KEY = "FMS8JYNTIQF7HBFM6G34P6SK52W6JQZKSM"
+const CONTRACT_ADDRESS = "0x514255d8f3387ac750e24776729d461fa8cb90d5";
+const deneme = "0x302955b74C969aA09bb270DAa775B65Fc9b7Bc29"
 
 function AfterSignupParent() {
 
-  
-    
-      window.scrollTo(0, 0)
 
 
-      const [kullanıcıName, setName] = useState("");
-      const [kullanıcıNameChild, setChildName] = useState("");
+  window.scrollTo(0, 0)
 
 
-     
+  const [kullanıcıName, setName] = useState("");
+  const [kullanıcıNameChild, setChildName] = useState("");
 
-      const navigate = useNavigate();
-      useEffect(() => {
-        WalletService.connect().then(() => {
-          WalletService.contract.getRole().then(async(role: string) => {
-            
-            if (role == 'Unregistered' || role=='Child') {
-              navigate('/login')
-              console.log('kayıtlıdegil')
- 
-            }else if (role == 'Parent'){
-              const parent = await WalletService.contract.getParent();
-              setName(parent.name);
-              console.log(parent.name)
-              const child = await WalletService.contract.getChildrenList();
-              
-              
 
-            }
-            
-          })
-    
-        })
-      }, []);
-        
-      
-      
-      
-      
-      
-      
-      
-      type MenuItem = Required<MenuProps>['items'][number];
-        function getItem(
-          label: React.ReactNode,
-          key: React.Key,
-          icon?: React.ReactNode,
-          children?: MenuItem[],
-          type?: 'group',
-        ): MenuItem {
-          return {
-            key,
-            icon,
-            children,
-            label,
-            type,
-          } as MenuItem;
+
+
+  const navigate = useNavigate();
+  useEffect(() => {
+    WalletService.connect().then(() => {
+      WalletService.contract.getRole().then(async (role: string) => {
+
+        if (role == 'Unregistered' || role == 'Child') {
+          navigate('/login')
+          console.log('kayıtlıdegil')
+
+        } else if (role == 'Parent') {
+          const parent = await WalletService.contract.getParent();
+          setName(parent.name);
+          console.log(parent.name)
+          const child = await WalletService.contract.getChildrenList();
         }
 
+        const etherscan = await axios.get(endpoint + `?module=account&action=txlist&address=${CONTRACT_ADDRESS}&startblock=0&endblock=99999999&page=1&offset=10&sort=asc&apikey=${ETHERSCAN_API_KEY}`)
+        let context = etherscan.data.result;
+        for (let i = 0; i < context.length; i++) {
+          if (context[i].to === CONTRACT_ADDRESS) {
+            console.log(context[i].to)
+          }
+
+        }
+      })
+
+    })
+  }, []);
 
 
 
-      const items: MenuItem[] = [
-        getItem(<Link to="/after_signup"><h2 style={{fontSize:'28px',color:'black',textAlign:'center',marginTop:'15px'}}>My CrytoBox</h2></Link>, '1', <WalletOutlined style={{position:'absolute',marginTop:'6px',fontSize:'35px'}} />),
-        getItem(<Link to="/cocuklarim"><h2 style={{fontSize:'28px',color:'black',textAlign:'center',marginTop:'15px'}}>Çocuklarım</h2></Link>, '2', <TeamOutlined style={{position:'absolute',marginTop:'6px',fontSize:'35px'}}/>),
-        getItem(<Link to="/login"><h2 style={{fontSize:'28px',color:'black',textAlign:'center',marginTop:'15px'}}>Oturum Değiştirme </h2></Link>, '3', <ExportOutlined style={{position:'absolute',marginTop:'6px',fontSize:'35px'}}/>),
-        
-
-      ];
 
 
-        return(
-        <div>
-
-            <Layout>
-              <Header style={{background:"white", padding:50,position:'sticky', top:0, zIndex: 100, boxShadow: '0px 0px 6px 0px rgba(0, 0, 0, 0.7)'}}>
-                <Avatar style={{position:'absolute', right:100, top:35,}} src= './coin2.png' />
-
-                <Link to="/"><Image style={{position:'absolute', left:30,top:-65}}width={100} src="./logo1.png" /></Link>
-               
-                <Button style={{position:'absolute', left:190, top:15, color:'#13C2C2',fontWeight:'bold',fontSize:'35px'}}
-                
-                type="link" danger><Link to="/">CryptoBox</Link></Button>
-
-                <TeamOutlined style={{position:'absolute',right:250,top:40,color:'#13C2C2',borderBlockColor:'#13C2C2' ,fontSize:'30px'}}/>
 
 
-              </Header>            
-                  <Layout>
 
-                  <Sider style={{padding:200,backgroundColor:'#13C2C2'}}>
-
-                    <Menu
-                      style={{ position:'absolute',left:0,top:50,backgroundColor:'#13C2C2'}}
-                      mode="inline"
-                      theme="dark"
-                      defaultSelectedKeys={['1']}
-                      items={items}
-                    />
-                  </Sider>
-
-
-              <Content style={{background:"white", 
-                padding:580, 
-                backgroundPosition: 'center',
-                backgroundSize: 'cover',
-                backgroundRepeat: 'no-repeat'}}>
-
-                <h2 style={{fontWeight:'bold', position:'absolute', left:450,top:130,color:'#13C2C2',fontSize:'35px'}}>HOŞ GELDİN</h2>
-                <h2 style={{position:'absolute', left:670,top:130,fontWeight:'bold',color:'#13C2C2',fontSize:'35px'}}>{kullanıcıName}</h2>
-                <h2 style={{position:'absolute', left:670,top:130,fontWeight:'bold',color:'#13C2C2',fontSize:'35px'}}>{kullanıcıNameChild}</h2>
-                <Avatar style={{position:'absolute', left:1190, top:230,width:'100px',height:'100px'}} src= './coin2.png' />
-
-                <Link to='/paragonderme'><h2 style={{position:'absolute', left:1250,top:450,color:'#13C2C2',fontSize:'20px'}}>Para Gönderme</h2></Link>
-                <Link to='/paragonderme'><SendOutlined style={{position:'absolute',left:1300,top:400,color:'#13C2C2',fontSize:'45px'}} /></Link>
-
-                <Link to='/paracekme'><h2 style={{position:'absolute', left:1100,top:450,color:'#13C2C2',fontSize:'20px'}}>Para Çekme</h2></Link>
-                <Link to='/paracekme'><VerticalAlignTopOutlined style={{position:'absolute',left:1130,top:400,color:'#13C2C2',fontSize:'45px'}} /></Link>
+  type MenuItem = Required<MenuProps>['items'][number];
+  function getItem(
+    label: React.ReactNode,
+    key: React.Key,
+    icon?: React.ReactNode,
+    children?: MenuItem[],
+    type?: 'group',
+  ): MenuItem {
+    return {
+      key,
+      icon,
+      children,
+      label,
+      type,
+    } as MenuItem;
+  }
 
 
-                
-                <h2 style={{fontWeight:'bold',position:'absolute', left:850,top:550,color:'#434343',fontSize:'30px'}}>Hesap Hareketleri</h2>
-                <Card.Grid style={{width:'900px', height:'100px',position:'absolute', left:800, top:620,backgroundColor:'#13C2C2'}}></Card.Grid>
-                <Card.Grid style={{width:'900px', height:'100px',position:'absolute', left:800, top:719,backgroundColor:'#13C2C2'}}></Card.Grid>
-                <Card.Grid style={{width:'900px', height:'100px',position:'absolute', left:800, top:818,backgroundColor:'#13C2C2'}}></Card.Grid>
-                <Card.Grid style={{width:'900px', height:'100px',position:'absolute', left:800, top:917,backgroundColor:'#13C2C2'}}></Card.Grid>
-                
-               </Content>
-               </Layout>
-               <Footer style={{background:"white", padding:60,boxShadow: '0px 0px 6px 0px rgba(0, 0, 0, 0.7)'}}>
-                  <h2 style={{position:'absolute', right:1000,top:1300}}> © 2022 Intertech, Inc. Her hakkı saklıdır</h2>
-                </Footer> 
-                
-            </Layout>
-        </div>
 
-        )
-        
-    
+
+  const items: MenuItem[] = [
+    getItem(<Link to="/after_signup"><h2 style={{ fontSize: '28px', color: 'black', textAlign: 'center', marginTop: '15px' }}>My CrytoBox</h2></Link>, '1', <WalletOutlined style={{ position: 'absolute', marginTop: '6px', fontSize: '35px' }} />),
+    getItem(<Link to="/cocuklarim"><h2 style={{ fontSize: '28px', color: 'black', textAlign: 'center', marginTop: '15px' }}>Çocuklarım</h2></Link>, '2', <TeamOutlined style={{ position: 'absolute', marginTop: '6px', fontSize: '35px' }} />),
+    getItem(<Link to="/login"><h2 style={{ fontSize: '28px', color: 'black', textAlign: 'center', marginTop: '15px' }}>Oturum Değiştirme </h2></Link>, '3', <ExportOutlined style={{ position: 'absolute', marginTop: '6px', fontSize: '35px' }} />),
+
+
+  ];
+
+
+  return (
+    <div>
+
+      <Layout>
+        <Header style={{ background: "white", padding: 50, position: 'sticky', top: 0, zIndex: 100, boxShadow: '0px 0px 6px 0px rgba(0, 0, 0, 0.7)' }}>
+          <Avatar style={{ position: 'absolute', right: 100, top: 35, }} src='./coin2.png' />
+
+          <Link to="/"><Image style={{ position: 'absolute', left: 30, top: -65 }} width={100} src="./logo1.png" /></Link>
+
+          <Button style={{ position: 'absolute', left: 190, top: 15, color: '#13C2C2', fontWeight: 'bold', fontSize: '35px' }}
+
+            type="link" danger><Link to="/">CryptoBox</Link></Button>
+
+          <TeamOutlined style={{ position: 'absolute', right: 250, top: 40, color: '#13C2C2', borderBlockColor: '#13C2C2', fontSize: '30px' }} />
+
+
+        </Header>
+        <Layout>
+
+          <Sider style={{ padding: 200, backgroundColor: '#13C2C2' }}>
+
+            <Menu
+              style={{ position: 'absolute', left: 0, top: 50, backgroundColor: '#13C2C2' }}
+              mode="inline"
+              theme="dark"
+              defaultSelectedKeys={['1']}
+              items={items}
+            />
+          </Sider>
+
+
+          <Content style={{
+            background: "white",
+            padding: 580,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+          }}>
+
+            <h2 style={{ fontWeight: 'bold', position: 'absolute', left: 450, top: 130, color: '#13C2C2', fontSize: '35px' }}>HOŞ GELDİN</h2>
+            <h2 style={{ position: 'absolute', left: 670, top: 130, fontWeight: 'bold', color: '#13C2C2', fontSize: '35px' }}>{kullanıcıName}</h2>
+            <h2 style={{ position: 'absolute', left: 670, top: 130, fontWeight: 'bold', color: '#13C2C2', fontSize: '35px' }}>{kullanıcıNameChild}</h2>
+            <Avatar style={{ position: 'absolute', left: 1190, top: 230, width: '100px', height: '100px' }} src='./coin2.png' />
+
+            <Link to='/paragonderme'><h2 style={{ position: 'absolute', left: 1250, top: 450, color: '#13C2C2', fontSize: '20px' }}>Para Gönderme</h2></Link>
+            <Link to='/paragonderme'><SendOutlined style={{ position: 'absolute', left: 1300, top: 400, color: '#13C2C2', fontSize: '45px' }} /></Link>
+
+            <Link to='/paracekme'><h2 style={{ position: 'absolute', left: 1100, top: 450, color: '#13C2C2', fontSize: '20px' }}>Para Çekme</h2></Link>
+            <Link to='/paracekme'><VerticalAlignTopOutlined style={{ position: 'absolute', left: 1130, top: 400, color: '#13C2C2', fontSize: '45px' }} /></Link>
+
+
+
+            <h2 style={{ fontWeight: 'bold', position: 'absolute', left: 850, top: 550, color: '#434343', fontSize: '30px' }}>Hesap Hareketleri</h2>
+            <Card.Grid style={{ width: '900px', height: '100px', position: 'absolute', left: 800, top: 620, backgroundColor: '#13C2C2' }}></Card.Grid>
+            <Card.Grid style={{ width: '900px', height: '100px', position: 'absolute', left: 800, top: 719, backgroundColor: '#13C2C2' }}></Card.Grid>
+            <Card.Grid style={{ width: '900px', height: '100px', position: 'absolute', left: 800, top: 818, backgroundColor: '#13C2C2' }}></Card.Grid>
+            <Card.Grid style={{ width: '900px', height: '100px', position: 'absolute', left: 800, top: 917, backgroundColor: '#13C2C2' }}></Card.Grid>
+
+          </Content>
+        </Layout>
+        <Footer style={{ background: "white", padding: 60, boxShadow: '0px 0px 6px 0px rgba(0, 0, 0, 0.7)' }}>
+          <h2 style={{ position: 'absolute', right: 1000, top: 1300 }}> © 2022 Intertech, Inc. Her hakkı saklıdır</h2>
+        </Footer>
+
+      </Layout>
+    </div>
+
+  )
+
+
 
 }
-export default AfterSignupParent; 
+export default AfterSignupParent;
 
 function openNotificationWithIcon(arg0: string) {
   throw new Error('Function not implemented.');
